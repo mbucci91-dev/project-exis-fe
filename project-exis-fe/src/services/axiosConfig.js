6f8059df5584d0d14147daca20ba30928ede1b3b
mbucci91-dev <bucci.marco.dev@gmail.com>
2025-12-09 16:24:49 +0100

    fix baseUrl

--- project-exis-fe/src/services/axiosConfig.js
+++ project-exis-fe/src/services/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Crea un'istanza axios personalizzata
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://project-exis-be.onrender.com',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://project-exis-be.onrender.com/api/',
   headers: {
     'Content-Type': 'application/json',
   },
